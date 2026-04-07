cdd441b29a69372f4938d6368068dca4681fb321
Arun-bits <arung2841@gmail.com>
2026-04-07 15:14:16 +0530

    Initial commit

--- src/main_pipeline.py
+++ src/main_pipeline.py
@@ -1,11 +1,9 @@
-# src/main_pipeline.py
-
 import time
 from src.log_collector import collect_authentication_logs
 from src.log_parser import parse_events
 from src.db_manager import initialize_database, insert_events
 from src.config import STREAM_INTERVAL
-
+from src.alert_engine import run_alert_engine
 
 
 def start_pipeline():
@@ -30,9 +28,11 @@ def start_pipeline():
                     f"MACHINE={event['machine_id']}"
                 )
 
+            # 🔥 Run alert engine after new logs are inserted
+            run_alert_engine()
+
         time.sleep(STREAM_INTERVAL)
 
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     start_pipeline()
-
